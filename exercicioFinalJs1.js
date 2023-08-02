cee2ba4be1e57f3d2c3409d020bf731931e7364e
Felipe Capello <felipe_capello74@hotmail.com>
2023-08-01 22:04:25 -0300

    Subindo fucntions

--- exercicioFinalJs1.js
+++ exercicioFinalJs1.js
@@ -2,6 +2,33 @@ vagas = []
 vaga = {}
 candidato = {}
 
+function listarVaga(){
+    for(i=0; i<vagas.length; i++){
+        alert(
+             (i+1) + "º"  + " Vaga " + "\n" +
+             vaga.nome + "\n" +
+             vaga.candidato        )
+    }
+}
+
+function cadastrarVaga(){
+    
+    vaga.nome = prompt("Digite o nome da vaga")
+            vaga.descricao = prompt("Digite a descrição da vaga")
+            vaga.dataLimite = prompt("Digite a data limite da vaga")
+            vaga.candidato = []
+            vagas.push(vaga)
+
+}
+
+function cadastrarCandidato(){
+    candidato.nome = prompt("Digite o nome do candidato")
+            candidato.indiceVaga = prompt("Digite o indice da vaga desejada")
+            for(i=0; i<vagas.lenght; i++){
+                    vaga.candidato.push(candidato)
+                }
+}
+
 do {
     choice = prompt("Digite a opção que deseja: \n 1 - Listar vagas disponíveis \n 2 - Criar uma nova vaga \n 3 - Visualizar uma vaga \n 4 - Inscrever um candidado em uma vaga \n 5 - Excluir uma vaga \n 6 - Sair ") 
 
@@ -9,30 +36,15 @@ do {
         
 
         case "1":
-            for(i=0; i<vagas.length; i++){
-                alert(
-                     (i+1) + "º"  + " Vaga " + "\n" +
-                     vaga.nome + "\n" +
-                     vaga.candidato
-                )
-            }
-            break
+        listarVaga();       
+        break
         case "2":
-            vaga.nome = prompt("Digite o nome da vaga")
-            vaga.descricao = prompt("Digite a descrição da vaga")
-            vaga.dataLimite = prompt("Digite a data limite da vaga")
-            vaga.candidato = []
-            vagas.push(vaga)
-            
-            break
+        cadastrarVaga();
+        break
         case "3":
 
         case "4":
-            candidato.nome = prompt("Digite o nome do candidato")
-            candidato.indiceVaga = prompt("Digite o indice da vaga desejada")
-            for(i=0; i<vagas.lenght; i++){
-                    vaga.candidato.push(candidato)
-                }
+        cadastrarCandidato();
         break
 
         case "5":
